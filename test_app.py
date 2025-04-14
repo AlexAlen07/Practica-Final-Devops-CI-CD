2ed07625af1a08ff67e22123a751dc1832509dba
AlenAlen27 <alenalen27je@gmail.com>
2025-04-14 18:46:24 -0400

    Ajusto el test para considerar la clase en el h1

--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
 def test_hola_mundo():
     with open("index.html", "r", encoding="utf-8") as f:
         contenido = f.read()
-    assert "<h1>Bienvenidos a la Práctica Final DevOps CI/CD</h1>" in contenido
+    assert "Bienvenidos a la Práctica Final DevOps CI/CD" in contenido
